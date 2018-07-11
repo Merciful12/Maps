b267707f787325ab028c136c81f87f3b2e4037aa
Merciful12 <future.12@yandex.ru>
2018-07-11 16:24:17 +0300

    add register page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-    <navbar></navbar>
-    <google-map class="content" />
     <router-view/>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
-import GoogleMap from './components/GoogleMap'
 
 export default {
-  name: 'App',
-  components: {
-    GoogleMap,
-    Navbar
-  }
+  name: 'App'
 }
 </script>
 
---
+++ client/src/components/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-container>
+    <b-row align-h="center">
+      <b-col cols="5">
+        <b-form @submit.prevent="onSubmit">
+            <b-form-input type="email"
+                          v-model="email"
+                          required
+                          placeholder="Enter email">
+            </b-form-input>
+
+            <b-form-input type="password"
+                          v-model="password"
+                          required
+                          placeholder="Enter password">
+            </b-form-input>
+          <b-button type="submit" variant="primary">Register</b-button>
+        </b-form>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import AuthService from '@/services/AuthService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      show: true
+    }
+  },
+  methods: {
+    async onSubmit (evt) {
+      const response = await AuthService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
